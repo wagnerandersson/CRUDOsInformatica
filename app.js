6d9131fb974878fe7782093bf5377a52fc836995
Wagner Andersson <wagner.andersson@gmail.com>
2021-05-23 14:48:20 -0300

    create database connection

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+
+
+const app = express()
+const port = process.env.API_PORT
+
+const routes = require('./routes')
+
+app.get('/', (req, res) => {
+    res.send('Hello Woeld!')
+});
+
+app.use(express.json());
+app.use(routes)
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "migrations",
+      directory: "database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+};
