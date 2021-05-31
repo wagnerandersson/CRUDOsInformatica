2d84c77464ecbbd685e06143b3cd7ded7054db6a
Wagner Andersson <wagner.andersson@gmail.com>
2021-05-31 20:16:01 -0300

    migrate e seeds at /

--- src/database/dbconfig.js
+++ src/database/dbconfig.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const knex = require("knex")({
   client: "pg",
   connection: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = knex;
